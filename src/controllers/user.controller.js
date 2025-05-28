bb8a935702712c1a7d0a186f209cc45b0826aa8b
Snehasishk <snehasishk23@gmail.com>
2025-05-28 17:38:18 +0530

    add new routes for user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -389,8 +389,9 @@ const getWatchHistory = asyncHandler(async (req, res) => {
   return;
   res
     .status(200)
-    .json(new ApiResponse(200, user[0].watHistory, "Watch history fetched"));
+    .json(new ApiResponse(200, user[0].watchHistory, "Watch history fetched"));
 });
+
 export {
   registerUser,
   loginUser,
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import {
+  changeCurrentPassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  getWatchHistory,
   loginUser,
   logOutUser,
   refreshAccessToken,
   registerUser,
+  updateCurrentUser,
+  updateUserAvatar,
+  updateUserCoverImage,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { userRegistrationValidator } from "../validator/index.js";
@@ -28,4 +35,15 @@ router.route("/register").post(
 router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT, logOutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateCurrentUser);
+router
+  .route("/update-avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/update-coverimage")
+  .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/watch-history").get(verifyJWT, getWatchHistory);
 export default router;
